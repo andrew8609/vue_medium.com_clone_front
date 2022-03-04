1c7d9ee6a755d1b18f2ea8a7be3a65199653edf3
ruslansobol <ruslansobol090@protonmail.com>
2022-03-03 17:13:17 -0800

    before test deploy

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app id="inspire">
+    <v-main>
+    <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+};
+</script>
+
+<style></style>
